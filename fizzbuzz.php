380284ae05a966b6858ac042068ed4877dd91932
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-13 10:58:35 -0500

    Added error message to user when they inputed something other than an integer

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -2,9 +2,14 @@
 
 //Step 1.
 fwrite(STDOUT, "Please enter a starting number! ");
-$min_number = (int) fgets(STDIN);
+$min_number = trim(fgets(STDIN));
 fwrite(STDOUT, "Now please enter an ending number! ");
-$max_num = (int) fgets(STDIN);
+$max_num = trim(fgets(STDIN));
+//Step 4.
+if(!is_numeric ($min_number) || !is_numeric ($max_num)){
+	echo "Error: Please insert numbers only\n";
+	(exit);
+}
 
 //Step 2.
 fwrite(STDOUT, "Enter a number to increase your starting number by ");
