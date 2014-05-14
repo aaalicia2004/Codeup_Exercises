34943eefc5f8b67d3b2c8469ceae286fa9bb1628
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-14 08:58:09 -0500

    Created an array of fruit with a while loop, for loop and foreach loop.

---
+++ fruit.php
@@ -0,0 +1,32 @@
+<?php
+
+$fruit = ["Star Fruit", "Grapefruit", "Kiwi", "Mango", "Coconut", "Apple"];
+$i = 0;
+
+//FIRST STEP: Create an array of fruit (3-5). Write a loop that prints the fruit.
+//Write as a for loop and as a foreach loop.
+
+//While Loop:
+
+//while($i < count($fruit)){
+//	echo $i+1 . ". ";
+//	echo $fruit[$i] . "\n";
+//	$i++;
+//}
+
+//For Loop:
+
+//for($i = 0; $i < count($fruit); $i++){
+//	echo $i+1 . ". ";
+//	echo $fruit[$i] . "\n";
+//}
+
+
+//ForEach Loop:
+
+//foreach($fruit as $key => $value) {
+//	echo $key + 1 . ". ";
+//	echo $value . "\n";
+//}
+
+
