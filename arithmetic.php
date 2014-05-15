322e97d240ccfaab50403ced3883817d55fc5b3a
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-15 10:00:51 -0500

    funtions for add,subtract,multiply,divide

---
+++ arithmetic.php
@@ -0,0 +1,22 @@
+<?php
+
+function add($a, $b){
+	echo $a + $b . PHP_EOL;
+}
+add(50,50);
+
+function subtract($a, $b){
+	echo $a - $b . PHP_EOL;
+}
+subtract(100,25);
+
+function multiply($a, $b){
+	echo $a * $b . PHP_EOL;
+}
+multiply(25,4);
+
+function divide($a, $b){
+	echo $a / $b . PHP_EOL;
+}
+divide(100,4);
+
