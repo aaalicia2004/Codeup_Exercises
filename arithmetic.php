ef24c2e61ac8f5b0cab2454c91a33d2270a5f4ad
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-15 13:58:04 -0500

    assigned an is_numeric funtion to each argument and displayed error if not numeric

--- arithmetic.php
+++ arithmetic.php
@@ -1,22 +1,53 @@
 <?php
 
 function add($a, $b){
-	echo $a + $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a + $b;
+	}else{
+		echo "Value entered is not numeric!";
+	}
+	echo PHP_EOL;
 }
 add(50,50);
 
 function subtract($a, $b){
-	echo $a - $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a - $b;
+	}else{
+		echo "Value entered is not numeric!";
+	}
+	echo PHP_EOL;
 }
 subtract(100,25);
 
 function multiply($a, $b){
-	echo $a * $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a * $b;
+	}else{
+		echo "Value entered is not numeric!";
+	}
+	echo PHP_EOL;
 }
 multiply(25,4);
 
 function divide($a, $b){
-	echo $a / $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a / $b;
+	}else{
+		echo "Value entered is not numeric!";
+	}
+	echo PHP_EOL;
+
 }
 divide(100,4);
+
+function modulo($a, $b){
+	if (is_numeric($a) && is_numeric($b)){
+		echo $a % $b;
+	}else{
+		echo "Value entered is not numeric!";
+	}
+	echo PHP_EOL;
+} 
+modulo(100, 50); //Note: the remainder of 100 divided by 50 is 0.
 
