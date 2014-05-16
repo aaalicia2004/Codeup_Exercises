f880e07929e4fcdf713266fc16b85c0397df6c41
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-16 10:30:49 -0500

    created a Numeric check function, divide by zero function, and added them into the add, multiply, divide, modulo, and subtract funtions

--- arithmetic.php
+++ arithmetic.php
@@ -1,10 +1,6 @@
 <?php
 
-function divide_0_error($a,$b){
-		return "ERROR: Please choose a number other than ZERO!";
-}
-
-function add($a, $b){
+function check_numeric($a,$b){
 	if(is_numeric($a) && is_numeric($b)){
 		return $a + $b;
 	}else{
@@ -15,60 +11,45 @@ function add($a, $b){
 		}
 	}
 }
+
+function divide_0_error($a,$b){
+	if($b==0){
+		echo "ERROR: Cannot divide by 0.";
+		return FALSE;
+	}
+}
+
+function add($a, $b){
+	if(check_numeric($a,$b)){
+			return $a+$b;
+	}
+}
 echo add(50,"Alicia") . PHP_EOL;
 
 function subtract($a, $b){
-	if (is_numeric($a) && is_numeric($b)){
+	if (check_numeric($a,$b)){
 		return $a - $b;
-	}else{
-		if(!is_numeric($a)){
-			return $a . " is not a number. DUH!";	
-		}else{
-			return $b . " is not a number. DUH!";
-		}
 	}
 }
 echo subtract(100,"Alicia") . PHP_EOL;
 
 function multiply($a, $b){
-	if (is_numeric($a) && is_numeric($b)){
+	if (check_numeric($a,$b)){
 		return $a * $b;
-	}else{
-		if(!is_numeric($a)){
-			return $a . " is not a number. DUH!";	
-		}else{
-			return $b . " is not a number. DUH!";
-		}
 	}
 }
 echo multiply(25,'Nope') . PHP_EOL;
 
 function divide($a, $b){
-	if($b==0){
-		return divide_0_error($a,$b);
-	}elseif(is_numeric($a) && is_numeric($b)){
+		if(divide_0_error($a,$b) && check_numeric($a, $b)){
 		return $a / $b;
-	}else{
-		if(!is_numeric($a)){
-			return $a . " is not a number. DUH!";	
-		}else{
-			return $b . " is not a number. DUH!";
-		}
 	}
 }
 echo divide(100,0) . PHP_EOL;
 
 function modulo($a, $b){
-	if ($b == 0){
-		return divide_0_error($a,$b);
-	}elseif(is_numeric($a) && is_numeric($b)){
+		if(divide_0_error($a,$b) && check_numeric($a,$b)){
 		return $a % $b;
-	}else{
-		if(!is_numeric($a)){
-			return $a . " is not a number. DUH!";	
-		}else{
-			return $b . " is not a number. DUH!";
-		}
 	}
 } 
 echo modulo(100,0) . PHP_EOL;
