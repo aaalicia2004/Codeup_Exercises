2286a0344bb79bf0955fced34859d6a691b54557
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-05-20 14:15:43 -0500

    turned a string into an array by creating a function

--- list_with_commas.php
+++ list_with_commas.php
@@ -4,7 +4,25 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 
 $physicists_array = explode(', ' , $physicists_string);
 
-print_r($physicists_array);
+
+function humanized_list($array)
+{
+$pop_and_push_element =  'and ' . array_pop($array);
+
+array_push($array, $pop_and_push_element);
+
+print_r($array);
+
+$string = implode(', ' , $array);
+return $string;
+}
+
+$famous_fake_physicists_string = humanized_list($physicists_array);
+
+echo "Some of the most famous fictional theoretical physicists are $famous_fake_physicists_string" . PHP_EOL;
+
+
+
 
 
 
