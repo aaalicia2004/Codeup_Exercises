fc4c1f957c7e59d48dc55c1257e17d54653e9f99
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-06-03 12:02:06 -0500

    reviewed FizzBuzz exercise using if/else statments

--- fizzbuzz2.php
+++ fizzbuzz2.php
@@ -3,20 +3,15 @@
 $x = 1;
 
 while($x <= 100){
-	switch($x){
-		case ($x % 3 == 0 && $x % 5 == 0) :
-			echo "FIZZ BUZZ" . PHP_EOL;
-			break;
-		case ($x % 5 == 0) :
-			echo "BUZZ" . PHP_EOL;
-			break;
-		case ($x % 3 == 0) :
-			echo "FIZZ" . PHP_EOL;
-			break;
-		default:
-			echo "{$x}" . PHP_EOL;
-			break;
-	}
+	if ($x % 3 == 0 && $x % 5 == 0){
+		echo "FIZZ BUZZ" . PHP_EOL;
+	}else if($x % 5 == 0){
+		echo "BUZZ" . PHP_EOL;
+	}else if ($x % 3 == 0){
+		echo "FIZZ" . PHP_EOL;
+	}else{
+		echo "{$x}" . PHP_EOL;
+	}	
 	$x++;
 }
 
