a8c044dd63d834b27873eb644430a9c1a481893d
aaalicia2004 <aaalicia2004@yahoo.com>
2014-06-25 13:40:35 -0500

    Update fizzbuzz2.php
    Re-created FizzBuzz to practice PHP coding.

--- fizzbuzz2.php
+++ fizzbuzz2.php
@@ -4,15 +4,14 @@ $x = 1;
 
 while($x <= 100){
 	if ($x % 3 == 0 && $x % 5 == 0){
-		echo "FIZZ BUZZ" . PHP_EOL;
+	    echo "FIZZ BUZZ" . PHP_EOL;
 	}else if($x % 5 == 0){
-		echo "BUZZ" . PHP_EOL;
+	    echo "BUZZ" . PHP_EOL;
 	}else if ($x % 3 == 0){
-		echo "FIZZ" . PHP_EOL;
+	    echo "FIZZ" . PHP_EOL;
 	}else{
-		echo "{$x}" . PHP_EOL;
+	    echo "{$x}" . PHP_EOL;
 	}	
 	$x++;
 }
-
 ?>
