ddd37a868a8466dcd3b3ec180b372b9056d16846
aaalicia2004 <aaalicia2004@yahoo.com>
2014-06-25 13:42:29 -0500

    Update for.php

--- for.php
+++ for.php
@@ -6,9 +6,10 @@ $min_number = trim(fgets(STDIN));
 fwrite(STDOUT, "Now please enter an ending number! ");
 $max_num = trim(fgets(STDIN));
 //Step 4.
-if(!is_numeric ($min_number) || !is_numeric ($max_num)){
-	echo "Error: Please insert numbers only\n";
-	(exit);
+if(!is_numeric ($min_number) || !is_numeric ($max_num))
+{
+    echo "Error: Please insert numbers only\n";
+    (exit);
 }
 
 //Step 2.
@@ -16,13 +17,14 @@ fwrite(STDOUT, "Enter a number to increase your starting number by ");
 $increase_min_number = (int) fgets(STDIN);
 
 //Step 3.
-if($increase_min_number== 0){
-	$increase_min_number = 1;
+if($increase_min_number== 0)
+{
+    $increase_min_number = 1;
 }
 
-for($a = $min_number; $a <= $max_num; $a += $increase_min_number){
-	echo "{$a}\n";
-
+for($a = $min_number; $a <= $max_num; $a += $increase_min_number)
+{
+    echo "{$a}\n";
 }
 
 
